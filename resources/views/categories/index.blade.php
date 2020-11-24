37e7aa686d09c41dad0907dd53a4464419ee10d5
Hosein <mohammadhussein@bvb.com>
2020-11-25 01:27:09 +0330

    initialize git

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<div class="d-flex justify-content-end mb-2 ">
+    <a href="{{ route('categories.create') }}" class="btn btn-success">Add Category</a>
+</div>
+
+<div class="card card-default">
+    <div class="card-header">
+        Categories
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <th>Name</th>
+            </thead>
+            <tbody>
+                @foreach ($categories as $categorie)
+                    <tr>
+                        <td>
+                            {{ $categorie->name }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+    
+@endsection
