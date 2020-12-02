f5ae4d48786f219f83f86743b87ff5da07bb712c
Hosein <mohammadhussein@bvb.com>
2020-12-02 04:01:33 +0330

    episod 14 is complete.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -96,8 +96,12 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        session()->flash('success', 'Post trashed successfully.');
+
+        return redirect(route('posts.index'));
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     //
+    use softDeletes;
+
     protected $fillable = [
         'title', 'description' , 'content', 'image', 'pubished_at' 
     ];
