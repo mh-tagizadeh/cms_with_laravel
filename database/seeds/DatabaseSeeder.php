471269a3a7f5c258bd81191c4b7a0114d443b46e
Mohammadhosein Tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-10-04 11:40:07 +0330

    add seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(CategoryTableSeeder::class);
         $this->call(UserTableSeeder::class);
-        // $this->call(PostsTableSeeder::class);
     }
 }
